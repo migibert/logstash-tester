ff04c9d8cd2529aa4094b62bf6cbdef3b515deb3
Nicolas L <nicolas.lassalle@zenika.com>
2016-09-01 11:34:11 +0200

    exit if logstash configuration is KO

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -8,6 +8,9 @@ if [[ $1 == "all" || $1 == "filters" ]]; then
     echo "###  RUN Logstash --configtest ################"
     if [[ $2 == "y" ]]; then
         logstash --configtest -f /etc/logstash/conf.d/
+        if [[ $? -ne 0 ]] ; then
+            exit $?
+        fi
     fi
 
     echo "###  RUN FILTER Tests  ####################"
